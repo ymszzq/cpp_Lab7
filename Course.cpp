a5784ff0b1b34ba73a2277fd792de532cbb41c27
ymszzq <gmziven@gmail.com>
2018-11-10 00:48:07 -0800

    a

--- Course.cpp
+++ Course.cpp
@@ -2,30 +2,41 @@
 
 using namespace std;
 
-Course::Course (string title, dayOfWeek day, unsigned int start_time, unsigned int finish_time) : title (title), day (day), start_time (start_time), finish_time (finish_time)
-{
-}
-Course::Course (const Course & m) : title (m.title), day (m.day), start_time (m.start_time), finish_time (m.finish_time)
-{
-}
 
-Course & Course::operator =(const Course & m) {
-    //TODO implement the operator
-    return *this;
-}
-bool Course::operator == (const Course & m) const {
-    //TODO implement the operator
-    return false;
-}
+Course::Course(string title, dayOfWeek day, unsigned int start_time, unsigned int finish_time) : title(title), day(day),
+                                                                                                 start_time(start_time),finish_time(finish_time) {}
 
-bool Course::operator < (const Course & m) const
-{
-    //TODO implement the operator
-    return false;
-}
+Course::Course(const Course &course) : title(course.title), day(course.day), start_time(course.start_time), finish_time(course.finish_time) {}
 
-ostream & operator << (ostream &os, const Course & m)
-{
-    //TODO implement the operator
+Course &Course::operator=(const Course &rhs) = default;
+
+
+bool Course::operator==(const Course &rhs) const {
+    return title == rhs.title && day == rhs.day && start_time == rhs.start_time && finish_time == rhs.finish_time;
+}
+bool Course::operator<(const Course &rhs) const {
+    if (day == rhs.day)
+        if (start_time < rhs.start_time)
+            return true;
+    return day < rhs.day;
+}
+ostream &operator<<(ostream &os, const Course &rhs) {
+    string d;
+    switch(rhs.day) {
+        case Course::MON:
+            d= "M";
+        case Course::TUE:
+            d= "T";
+        case Course::WED:
+            d= "W";
+        case Course::FRI:
+            d= "F";
+        default:
+            break;
+    }
+    os << rhs.title;
+    os << " " << d;
+    os << " " << rhs.start_time;
+    os << " " << rhs.finish_time;
     return os;
 }
--- main.cpp
+++ main.cpp
@@ -5,29 +5,59 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 #include "Course.hpp"
 
 using namespace std;
 
-/*
- * print schedule - outputs course schedule to the screen
- * @param - add any new parameters you need
- */
-void printSchedule()
-{
-    //TODO implement your print using the copy algorithm, 2 iterators, and an ostream_iterator
+void printSchedule(vector<Course> course_list) {
+    vector<string> course_list_strings{};
+    copy(course_list.begin(), course_list.end(), ostream_iterator<Course>(cout,"\n"));
+    cout<<"--------------------";
 }
 
-int main () {
-    //TODO read from courses.txt
-    //TODO store data in an STL container
-    //TODO sort your STL container with the sort algorithm
-    //TODO implement code to determine schedule conflicts
-    //TODO print out schedule conflicts
-    //TODO print out schedule
-    cout << "Find the TODOs in the code and implement them." << endl;
-    cout << "Add/modify any functions/code you need to complete your task." << endl;
+int main() {
+    vector<Course> course_list;
+    ifstream input{"../courses.txt"};
+    if (!input.is_open()) {
+        cerr << "file not open" << endl;
+        exit(1);
+    }
+
+
+    string line,title,day;
+    Course::dayOfWeek dayOfWeek;
+    unsigned int start, finish;
+    while (getline(input, line)) {
+        istringstream iss{line};
+        iss >> title >> day >> start >> finish;
+
+        if (day == "M")
+            dayOfWeek = Course::dayOfWeek::MON;
+        if (day == "T")
+            dayOfWeek = Course::dayOfWeek::TUE;
+        if (day == "W")
+            dayOfWeek = Course::dayOfWeek::WED;
+        if (day == "F")
+            dayOfWeek = Course::dayOfWeek::FRI;
+        Course new_course{title, dayOfWeek, start, finish};
+        course_list.push_back(new_course);
+    }
+
+
+    sort(course_list.begin(), course_list.end());
+
+    for (auto iterator1 = course_list.begin(); iterator1 != course_list.end(); ++iterator1) {
+        for (auto iterator2 = iterator1 + 1; iterator2 != course_list.end(); ++iterator2) {
+            if (iterator2->start_time < iterator1->finish_time && iterator1->day == iterator2->day) {
+                cout<<"CONFLICT:\n"<<*iterator1<<"\n"<<*iterator2<<endl;
+            }
+        }
+    }
+
+    cout<<"--------------------------\n";
+
+    printSchedule(course_list);
     return 0;
 }
